fc33e970a31f44ad0511154e0bd464a18ad4281a
Krystian <krychan144@gmail.com>
2018-04-19 19:52:14 +0200

    UserAuthenticate,

--- WebApiApp/WebApiApp/App_Start/WebApiConfig.cs
+++ WebApiApp/WebApiApp/App_Start/WebApiConfig.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace WebApiApp
 {
+    public class CustomJosonFormatter : JsonMediaTypeFormatter
+    {
+        public CustomJosonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+             base.SetDefaultContentHeaders(type,headers,mediaType);
+            headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+    }
+
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -19,6 +38,12 @@ namespace WebApiApp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Add(new CustomJosonFormatter());
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+           // config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //   config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
--- WebApiApp/WebApiApp/Controllers/EmpolyeesController.cs
+++ WebApiApp/WebApiApp/Controllers/EmpolyeesController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using EmployeeDataAccess;
 
 namespace WebApiApp.Controllers
 {
+     [EnableCorsAttribute("*","*","*")]
     public class EmployeesController : ApiController
     {
-        public IEnumerable<Employee> Get()
+        [BasicAuthentication]
+        public HttpResponseMessage Get(string gender = "All")
+        {
+            string username = Thread.CurrentPrincipal.Identity.Name;
+
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                switch (username.ToLower())
+                {
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.Where(e => e.Gender.ToLower() == "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK,
+                            entities.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
+                    default:
+                        return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+            }
+        }
+
+      /*  public IEnumerable<Employee> Get()
         {
             using (EmployeeDBEntities entites = new EmployeeDBEntities())
             {
@@ -24,6 +48,6 @@ namespace WebApiApp.Controllers
             {
                 return entities.Employees.FirstOrDefault(e => e.ID == id);
             }
-        }
+        }*/
     }
 }
