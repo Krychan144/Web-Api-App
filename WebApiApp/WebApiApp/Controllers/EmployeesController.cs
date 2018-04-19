84d43f9e26207e457c4f70874cda0308a1318be7
Krystian <krychan144@gmail.com>
2018-04-19 15:34:04 +0200

    Connect to Employee Data Base, Training

---
+++ WebApiApp/WebApiApp/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebApiApp.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employees> Get()
+        {
+            using(EmployeeDBEntites entites = new EmployeeDBEntites())
+            {
+                return entites.Employees.ToList();
+            }
+        }
+
+        public EmployeesGet()
+        {
+            using (EmployeeDBEntites entites = new EmployeeDBEntites())
+            {
+                return entites.Employees.FirstOfDefault(e => e.ID == id);
+            }
+        }
+    }
+}
